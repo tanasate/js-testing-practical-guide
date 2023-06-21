1503730afa82c02e9a6141c4cccf0f4e1a22b3cd
tanasate <tanasate.dev@gmail.com>
2023-06-21 21:35:18 +0700

    Defining Behaviors and Fixing Errors In Code

--- basic-testing-nodejs-esmodules/src/math.test.js
+++ basic-testing-nodejs-esmodules/src/math.test.js
@@ -13,3 +13,21 @@ test('should summarize all number value in an array', () => {
     expect(result).toBe(6)
 
 })
+
+test('should yeild NaN if a least one invalid number of providered', () => {
+
+    const inputs = ['invalid value', 1]
+
+    const result = add(inputs)
+
+    expect(result).toBeNaN()
+})
+
+test('should yeild a correct sum if an array of numeric string values is provided', () => {
+
+    const numbers = ['1', '2']
+
+    const result = add(numbers)
+
+    expect(result).toBe(3)
+})
