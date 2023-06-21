a5471fdd097740b5c18626441da7d78f2207f4ad
tanasate <tanasate.dev@gmail.com>
2023-06-21 21:04:00 +0700

    The AAA Pattern

--- basic-testing-nodejs-esmodules/src/math.test.js
+++ basic-testing-nodejs-esmodules/src/math.test.js
@@ -2,6 +2,14 @@ import { test, expect } from 'vitest'
 import { add } from './math'
 
 test('should summarize all number value in an array', () => {
-    const result = add([1, 2, 3])
+
+    // Arrange
+    const numbers = [1, 2, 3]
+
+    // Act
+    const result = add(numbers)
+
+    // Assert
     expect(result).toBe(6)
+
 })
